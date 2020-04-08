336e921578def012a720d6ad53610f7a63eff092
panyapoc <panyapol.chu@gmail.com>
2020-04-08 21:16:55 +0700

    [CF] added onChannelStateChange [CF] added SNS [CF] added CW event

--- lambda/event/onChannelStateChange/onChannelStateChange.py
+++ lambda/event/onChannelStateChange/onChannelStateChange.py
@@ -1,29 +1,36 @@
 # CloudWatch Channel State Change Event idle -> Running
 # 1. Update Channel State on DDB
-# 2. Notify Mobile Channel Ready
+# 2. Update VoD table
+# 3. Notify Mobile Channel Ready
 
 # CloudWatch Channel State Change Event Running -> Idle
 # 1. Update Channel State on DDB
 # 2. Start moving archive file to new location
 # 3. Create .m3u8 file from list of .ts file
 # 4. Add new .m3u8 to DDB for future playing
+# 5. Update VoD table
 
 import boto3
 import json
 import os
+import uuid
 
 # BOTO3
 medialive = boto3.client('medialive')
 dynamodb = boto3.resource("dynamodb")
+sns = boto3.resource("sns")
 
 ddb_channel = dynamodb.Table(os.environ['ddb_channel'])
+ddb_vod = dynamodb.Table(os.environ['ddb_vod'])
 
 def lambda_handler(event, context):
     # CloudWatch Channel State Change Event idle -> Running
     # 1. Update Channel State on DDB
-    # 2. Notify Mobile Channel Ready
+    # 2. Update VoD table
+    # 3. Notify Mobile Channel Ready
     if event['detail']['state'] == 'RUNNING' :
         ChannelID = getChannelID(event['detail']['channel_arn'])
+        print(f'updating channel {ChannelID} State to RUNNING')
         ddb_channel.update_item(
             Key={
                 'ChannelID': ChannelID
@@ -33,12 +40,35 @@ def lambda_handler(event, context):
                 '#keyState' : 'State',
             },
             ExpressionAttributeValues={
-                ':State': medialive_stop_channel['State']
+                ':State': 'RUNNING'
             }
         )
 
-        return 'ok'
+        VoD = {
+            'VoDID' : str(uuid.uuid4()),
+            'ChannelId' : '',
+            'StartTime' : '',
+            'EndTime' : '',
+            'VoDEndPoint' : '',
+            'Streamer' : ''
+        }
+        ddb_vod.putItem(Item=VoD)
 
+        Channel = ddb_channel.get_item(
+            Key={ 'ChannelID': ChannelID }
+        )
+        ChannelRTMPEndpoint = Channel['Item']['RTMPEndpoint']
+        Message = {
+            'Message' : f'Channel {ChannelID} is ready',
+            'RTMPEndpoint' : ChannelRTMPEndpoint
+        }
+        print(Message)
+        sns_publish = sns.publish(
+            TopicArn=os.environ['snstopic'],
+            Message=Message,
+            MessageStructure='string',
+        )
+        return 'ok'
 
 
     # CloudWatch Channel State Change Event Running -> Idle
@@ -46,6 +76,7 @@ def lambda_handler(event, context):
     # 2. Start moving archive file to new location
     # 3. Create .m3u8 file from list of .ts file
     # 4. Add new .m3u8 to DDB for future playing
+    # 5. Update VoD table
     elif event['detail']['state'] == 'STOPPED' :
         return 'ok'
 
