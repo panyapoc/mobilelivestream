fd8d50fded6d025d88fe57b35a700bc11fb72c12
panyapoc <panyapol.chu@gmail.com>
2020-04-11 01:14:45 +0700

    [ui] added vue router

--- webui/src/main.js
+++ webui/src/main.js
@@ -1,7 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
-import VueClipboard from 'vue-clipboard2'
+import VueClipboard from 'vue-clipboard2';
+import VueRouter from 'vue-router';
 import axios from "axios";
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -10,6 +11,7 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 Vue.use(VueClipboard);
+Vue.use(VueRouter);
 Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false;
