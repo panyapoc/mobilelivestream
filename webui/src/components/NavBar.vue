6ead6df596e4610b731b61d8fab7f57318439e58
panyapoc <panyapol.chu@gmail.com>
2020-04-10 15:58:19 +0700

    [ui] nav v1

---
+++ webui/src/components/NavBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="NavBar">
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="primary">
+        <b-navbar-brand href="#">NavBar</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav> -->
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#" disabled>Readme</b-nav-item>
+            <b-nav-item target="_blank" rel="noopener noreferrer" href="https://github.com/panyapoc/mobilelivestream">GitHub</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar',
+    // props: {
+    //   msg: String
+    // }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
