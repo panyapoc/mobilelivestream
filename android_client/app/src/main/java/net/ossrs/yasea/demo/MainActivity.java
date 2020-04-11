f1339000511af7be01343d92bb63ea59ae07e7ab
panyapoc <panyapol.chu@gmail.com>
2020-04-11 23:55:09 +0700

    [android] Start Channel chk point

--- android_client/app/src/main/java/net/ossrs/yasea/demo/MainActivity.java
+++ android_client/app/src/main/java/net/ossrs/yasea/demo/MainActivity.java
@@ -1,10 +1,19 @@
 package net.ossrs.yasea.demo;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
+import org.json.*;
 
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -51,10 +60,14 @@ public class MainActivity extends AppCompatActivity implements RtmpHandler.RtmpL
     private SrsPublisher mPublisher;
     private SrsCameraView mCameraView;
 
-    private String channelID;
+    private String channelId;
 
     private int mWidth = 640;
     private int mHeight = 480;
+
+    OkHttpClient client = new OkHttpClient();
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    public static final String ROOTAPI = "https://d3btskzrn2dzma.cloudfront.net/Prod";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -134,9 +147,13 @@ public class MainActivity extends AppCompatActivity implements RtmpHandler.RtmpL
             @Override
             public void onClick(View view) {
                 if(btnStartStopCH.getText().toString().equals("STARTCH")){
-
-//                    API startChannel
-//                    update rtmpUrl
+                    String url= ROOTAPI+"/channel/startChannel";
+                    String postBody="{}";
+                    try {
+                        startChannel(url,postBody);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 //                    mPublisher.pausePublish();
                     btnStartStopCH.setText("STOPCH");
                 }else{
@@ -180,6 +197,40 @@ public class MainActivity extends AppCompatActivity implements RtmpHandler.RtmpL
                     mPublisher.switchToHardEncoder();
                     btnSwitchEncoder.setText("soft encoder");
                 }
+            }
+        });
+    }
+
+    void startChannel(String postUrl,String postBody) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON, postBody);
+
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            public void onResponse(Call call, Response response) throws IOException {
+                String jsonString = response.body().string();
+                Log.d("TAG",jsonString);
+                JSONObject obj = null;
+                try {
+                    obj = new JSONObject(jsonString);
+                    rtmpUrl = obj.getString("RTMPEndpoint");
+                    channelId = obj.getString("ChannelId");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+//                Toast.makeText(getApplicationContext(), rtmpUrl, Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("rtmpUrl", rtmpUrl);
+                editor.apply();
             }
         });
     }
